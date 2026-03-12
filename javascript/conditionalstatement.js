416a967540045196ac1eebc60e74c1ff60f5c7d7
Sadhik-Hussain968 <sangapatnamsadhik2470@gmail.com>
2026-03-12 12:21:49 +0530

    Add files via upload

---
+++ javascript/conditionalstatement.js
@@ -0,0 +1,8 @@
+let x=1000;
+if(x>100){
+console.log("x is greaterthan 100");
+}
+else{
+console.log("x is lessthan 100");
+
+}
---
+++ javascript/ifstatement.js
@@ -0,0 +1,19 @@
+// let age=28;
+// let d1=false
+// if(age>18||d1==true){
+//     console.log("you can drive ")
+// }
+// else{
+//     console.log("you can't drive")
+// }
+let adhar=false;
+let pan=false;
+if (adhar){
+    console.log("you can open you bank account -using adhar")
+}
+else if (pan){
+    console.log("you can open your bank account-using pan")
+}
+else{
+    console.log("you can't open your bank account")
+}
